2388ba66583a4bada088224c4927bc126911941e
RayanH21 <rayan121.haddou@gmail.com>
2025-05-13 15:12:50 +0200

    Unit test XML

--- tests/test_heartbeat.py
+++ tests/test_heartbeat.py
@@ -4,13 +4,20 @@ import os
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from heartbeat.heartbeat import get_heartbeat_message
+from heartbeat.heartbeat import get_heartbeat_message, dict_to_xml  # ✅ nieuwe import
 
 class TestHeartbeatMessage(unittest.TestCase):
 
     def test_xml_contains_servicename(self):
         xml = get_heartbeat_message()
         self.assertIn("<ServiceName>frontend</ServiceName>", xml)
+
+
+class TestXMLGeneration(unittest.TestCase):
+
+    def test_dict_to_xml_contains_servicename(self):
+        xml = dict_to_xml({'ServiceName': 'test-service'})
+        self.assertIn('<ServiceName>test-service</ServiceName>', xml)
 
 if __name__ == "__main__":
     unittest.main()
