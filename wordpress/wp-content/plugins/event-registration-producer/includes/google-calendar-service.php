70dd5cf6f3a5893fea512ca7ddb0de49b7ba5007
RayanH21 <rayan121.haddou@gmail.com>
2025-05-22 21:01:04 +0200

    Pad veranderd

--- wordpress/wp-content/plugins/event-registration-producer/includes/google-calendar-service.php
+++ wordpress/wp-content/plugins/event-registration-producer/includes/google-calendar-service.php
@@ -1,13 +1,10 @@
-require_once __DIR__ . '/../../vendor/autoload.php'; // Pad aanpassen indien nodig
+<?php
+require_once __DIR__ . '/../../../vendor/autoload.php'; // Correct pad naar root/vendor
 
 function get_google_calendar_service() {
     $client = new \Google_Client();
     $client->setAuthConfig(__DIR__ . '/../credentials/calendar-service-account.json');
-
-
     $client->addScope('https://www.googleapis.com/auth/calendar');
-
-
     $client->setSubject('frontend@youmnimalha.be');
 
     return new Google_Service_Calendar($client);
