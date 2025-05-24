1c9aba3c91e708dd4d6780bb1923dd740d75f877
RayanH21 <rayan121.haddou@gmail.com>
2025-05-24 23:39:13 +0200

    Errors laten zien

--- wordpress/wp-content/plugins/event-registration-producer/event-registration-producer.php.php
+++ wordpress/wp-content/plugins/event-registration-producer/event-registration-producer.php.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 /**
  * Plugin Name: Event Registration Producer
  * Description: Toon events en verwerk inschrijvingen met RabbitMQ & Google Calendar.
